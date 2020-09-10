278a1b0d8a4f201d514a26bb49381c92509780ac
Slawomir Malczewski <slawekmalczewski@hotmail.co.uk>
2020-09-10 22:55:50 +0100

    layout and style changes

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,6 @@
 class Blog < ApplicationRecord
   has_rich_text :body
+  has_one_attached :main_poster
 
   scope :order_by_newest_ad, -> { order(created_at: :desc)}
   scope :order_by_valid_from, -> { order(valid_from: :asc)}
